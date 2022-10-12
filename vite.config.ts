846973f3601f2d64f04eb2b697e8fd82786e1327
tu.na <tu.na@deha-soft.com>
2022-10-12 09:27:51 +0700

    Config eslint

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    server: {
+      port: parseInt(process.env.VITE_PORT)
+    },
+    plugins: [react()]
+  });
+}
