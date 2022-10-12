64b388f07c3e986877c50f1586b5ab4ead8a0789
tu.na <tu.na@deha-soft.com>
2022-10-12 09:57:58 +0700

    Config i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-
+import { useTranslation } from 'react-i18next';
+import { translations } from './locales/translations';
 function App() {
+  const { t } = useTranslation();
   const [count, setCount] = useState(0);
 
   return (
@@ -15,7 +17,7 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{t(translations.title)}</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
